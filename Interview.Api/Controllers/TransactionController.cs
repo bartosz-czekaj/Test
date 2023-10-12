a1889e867812f06cb8c0e866898366b0b2f6e665
mikekgs@gmail.com <mikekgs@gmail.com>
2023-10-12 14:23:15 +0100

    So since you didn't want me to use the MVC version existing I've made things clearer by useing an ASP.Net web api project so you can test the API with Swagger which should just run up after a build.
    I hope I've understood the assignment

---
+++ Interview.Api/Controllers/TransactionController.cs
@@ -0,0 +1,50 @@
+﻿using Interview.DAL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interview.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private iTransaction dataAccess;
+
+        public TransactionController(iTransaction transactionService)
+        {
+            dataAccess = transactionService;
+        }
+
+        // GET api/<controller>
+        [HttpGet(Name = "GetTransaction")]
+        public Interview.DataModel.Transaction Get(string id)
+        {
+            var guid = new Guid(id);
+            return dataAccess.Retrieve(guid);
+        }
+
+        // POST api/<controller>
+        [HttpPost(Name = "PostTransaction")]
+        public void Post([FromBody] Interview.DataModel.Transaction record)
+        {
+            dataAccess.Create(record);
+        }
+
+        // PUT api/<controller>
+        [HttpPut(Name = "PutTransaction")]
+        public void Put([FromBody] Interview.DataModel.Transaction record)
+        {
+            dataAccess.Update(record);
+        }
+
+        // DELETE api/<controller>
+        [HttpDelete(Name = "DeleteTransaction")]
+        public void Delete(string id)
+        {
+            var guid = new Guid(id);
+            dataAccess.Delete(guid);
+        }
+    }
+}
---
+++ Interview/Transaction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Interview
+{
+    public class Transaction : ApiController
+    {
+        private Interview.DAL.Retrieve.DataFile dataAccess => new Interview.DAL.Retrieve.DataFile();
+
+        // GET api/<controller>
+        public IEnumerable<Interview.DataModel.Transaction> Get()
+        {
+            return dataAccess.RetrieveAll();
+        }
+
+        // GET api/<controller>
+        public Interview.DataModel.Transaction Get(string id)
+        {
+            var guid = new Guid(id);
+            return dataAccess.Retrieve(guid);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] Interview.DataModel.Transaction record)
+        {
+            dataAccess.Create(record);
+        }
+
+        // PUT api/<controller>
+        public void Put([FromBody] Interview.DataModel.Transaction record)
+        {
+            dataAccess.Update(record);
+        }
+
+        // DELETE api/<controller>
+        public void Delete(string id)
+        {
+            var guid = new Guid(id);
+            dataAccess.Delete(guid);
+        }
+    }
+}
